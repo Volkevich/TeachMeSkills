7b472066f0299b963145807b3f57ee48b7adaff7
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-04-17 20:38:26 +0300

    Lesson_1

---
+++ Lesson_1/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Первый аргумент - %S, \nВторой Аргумент - %S", args[0],args[1]);
+    }
+}
