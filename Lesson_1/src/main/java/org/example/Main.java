1348ff8436a45709d8c0d0c2e11e989b0da63eb6
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-04-17 22:06:57 +0300

    add var type

--- Lesson_1/src/main/java/org/example/Main.java
+++ Lesson_1/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Первый аргумент - %S, \nВторой Аргумент - %S", args[0],args[1]);
+        var x = 10L;
+        System.out.println(x);
     }
 }
