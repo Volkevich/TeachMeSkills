81527c4283d79cdc12ce88b4a76a4182058a5b90
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-04-22 19:38:25 +0300

    5 tasks have been implemented (add task)

--- Lesson 2 Task 1/src/main/java/org/example/Task1.java
+++ Lesson 2 Task 1/src/main/java/org/example/Task1.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import java.util.*;
 import java.util.stream.IntStream;
 
@@ -10,23 +11,20 @@ public class Task1 {
      * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
      */
     public static void main(String[] args) {
-        try {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите целое число: ");
             int num = scanner.nextInt();
             if (isPrime(num) ) {
-                String reversNumb = new StringBuilder(String.valueOf(num)).reverse().toString();
-                System.out.println("Ваше число в обратном порядке - " + reversNumb);
-            } else {
-                System.out.println("Ошибка!\nВведенное число не является целым!");
+                String result = new StringBuilder(String.valueOf(num)).reverse().toString();
+                System.out.println("Ваше число в обратном порядке - " + result);
             }
-        }catch (NumberFormatException e){
-            e.printStackTrace();
-        }
-
-
     }
+
     public static boolean isPrime(final int number) {
         return IntStream.rangeClosed(2, number / 2).anyMatch(i -> number % i == 0);
     }
+
+
+
+
 }
--- Lesson 2 Task 1/src/main/java/org/example/Task3.java
+++ Lesson 2 Task 1/src/main/java/org/example/Task3.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class Task3 {
     /**
      * Простая (2 балла)
-     *
+     * <p>
      * Пользователь задает целое число, больше или равно 100 (например, 3801).
      * Определить третью цифру справа в этом числе (в данном случае 8).
      */
     public static void main(String[] args) {
         System.out.println("Введите целое число: ");
         Scanner scanner = new Scanner(System.in);
-        Integer number = scanner.nextInt();
+        int number = scanner.nextInt();
 
         if (number >=100){
             int result = (number / 100) % 10;
             System.out.println("Третья цифра справа: - " + result);
         }
+
 
     }
 }
