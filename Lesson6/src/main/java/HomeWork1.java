01401c47482d9785508e45a56ec292ff3eb04578
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-05-14 14:42:24 +0300

    add вывод повторяющегося элемента + int -> char

--- Lesson6/src/main/java/HomeWork1.java
+++ Lesson6/src/main/java/HomeWork1.java
@@ -5,7 +5,7 @@ public class HomeWork1 {
      * Вывести на экран количество элементов самой длиной
      *  последовательности и номер элемента, который является ее началом.*/
     public static void main(String[] args) {
-        int[] arr = {2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5};
+        char[] arr = {2, 2, 2, 3, 'c', 'c', 'c', 'c', 'c', 'c', 5, 5, 5, 5, 5, 5};
         int startIndex = 0;
         int countElements = 1;
         int maxStartIndex = 0;
@@ -25,6 +25,7 @@ public class HomeWork1 {
 
         System.out.println("Количество элементов самой длиной последовательности: " + maxLength);
         System.out.println("Номер элемента, который является ее началом: " + maxStartIndex);
+        System.out.println("Элемент который повторяется - '" + arr[maxStartIndex] + "'");
 
     }
 
